6fbc9bb398482d46cafb68b858af94befff17b48
LeeAain <leeaain2020@gmail.com>
2021-12-28 01:29:56 +0900

    3

---
+++ JS-coplit/recursion_factorial.js
@@ -0,0 +1,16 @@
+// n-factorial 값을 리턴하라
+// n!은 1부터 n까지 1씩 증가한 모든 값의 곱이다
+// ! my solv.
+function factorial(num) {
+    if (num === 0) {return 1}
+    return num * factorial(num -1)
+}
+
+// the best solv.
+function factorial(num) {
+    if (num <= 1) {
+      return 1;
+    }
+  
+    return num * factorial(num - 1);
+  }
