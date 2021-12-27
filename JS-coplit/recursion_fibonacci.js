fb8c32d47c947a5459058f0234a77ef1de3f1984
LeeAain <leeaain2020@gmail.com>
2021-12-28 01:36:30 +0900

    4th

--- JS-coplit/recursion_fibonacci.js
+++ JS-coplit/recursion_fibonacci.js
@@ -3,4 +3,17 @@
 // 1번째 피보나치 수 === 1
 // 2번째 피보나치 수부터 바로 직전의 두 피보나치 수의 합
 // ! my code.
-
+function fibonacci(num) {
+    if (num === 0) {return 0}
+    if (num === 1) {return 1}
+    return fibonacci(num -1) + fibonacci(num -2)
+  }
+  
+  // the best code.
+  function fibonacci(num) {
+    if (num <= 1) {
+      return num;
+    }
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
