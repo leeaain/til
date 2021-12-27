6ac89dd1cc1155ff28a87cca1878f879ef66273a
LeeAain <leeaain2020@gmail.com>
2021-12-28 01:39:04 +0900

    5th

---
+++ JS-coplit/recursion5_arrSum.js
@@ -0,0 +1,15 @@
+// 배열의 모든 요소의 합을 리턴하라
+// ! my code
+function arrSum(arr) {
+    if (arr.length === 0) { return 0 }
+    return arr[0] + arrSum(arr.slice(1))
+  }
+  
+// the best code.
+function fibonacci(num) {
+    if (num <= 1) {
+      return num;
+    }
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
