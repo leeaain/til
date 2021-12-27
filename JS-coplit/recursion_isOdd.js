7ff869fe3cc1d9f60bed3f18f54a58ccb1d64bd5
LeeAain <leeaain2020@gmail.com>
2021-12-28 01:26:57 +0900

    1st

---
+++ JS-coplit/recursion_isOdd.js
@@ -0,0 +1,22 @@
+// num이 홀수인지 여부를 리턴하라
+// !my solv.
+function isOdd(num) {
+    if (num < 0) { num = -num}
+    if (num <= 1) { return (num === 1)}
+    // num-2로 재귀함수를 돌린다.
+    return isOdd(num -2)
+  }
+
+  // the best solv.
+  function isOdd(num) {
+    if (num === 0) {
+      return false;
+    } else if (num === 1) {
+      return true;
+    }
+  
+    if (num < 0) {
+      return isOdd(-num);
+    }
+    return isOdd(num - 2);
+  }
